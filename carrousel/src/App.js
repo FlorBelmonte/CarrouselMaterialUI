ccb76d2da9f7d5fd7ff3bfb8fc4e3d77ee6b1e24
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2023-10-09 18:57:34 -0300

    se creó card con MaterialIU

--- carrousel/src/App.js
+++ carrousel/src/App.js
@@ -1,12 +1,24 @@
+import { Container, Paper, Typography } from '@mui/material';
 import './App.css';
+// import MediaCard from './components/Card';
 import Carrousel from './components/Carrousel'
 
 function App() {
   return (
-<div className='App'>
+    <Container maxWidth= "lg">
+      <Paper elevation = {3} style = {{padding: '20px', marginTop: '20px'}}>
+
+    <div className='App'>
+    <Typography variant="h4" align="center">
+          Carrusel de Imágenes con Material-UI
+        </Typography>
     <Carrousel/>
-</div>
+    {/* <MediaCard/> */}
+    </div>
+    </Paper>
+    </Container>
   );
 }
+
 
 export default App;
